428e21da3cc6807a6c0dc3cf84bd394b4d0fd3e1
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-04-24 13:19:21 -0600

    up cv_2 v2: v5

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: ['my-profile-wk2a.onrender.com'], // 👈 aquí tu dominio de Render
+  },
+});
