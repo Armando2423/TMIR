75b87d26948346aed2416608afb2a944af5fb5ee
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-04-24 12:24:39 -0600

    proyect my OWN_CV v6

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  preview: {
     port: 3000,
     host: true,
-    allowedHosts: ['https://my-profile-wk2a.onrender.com'],
+    allowedHosts: ['https://my-profile-wk2a.onrender.com'], // 👈 aquí tu dominio de Render
   },
-})
+});
