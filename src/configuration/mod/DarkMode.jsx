1728c49788da6d0e3e8aec96aeae30114b14a160
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-04-24 12:02:45 -0600

    Fix: corrige importación de DarkMode.css v2

--- src/configuration/mod/DarkMode.jsx
+++ src/configuration/mod/DarkMode.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { IoIosSunny } from "react-icons/io";
 import { MdOutlineDarkMode } from "react-icons/md";
-import './DarkMode.css';
+
 
 export default function DarkMode() {
     const [darkMode, setDarkMode] = useState(() => {
