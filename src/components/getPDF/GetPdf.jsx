d27001a1536537138450981c7d27d18cbdb26593
Armando2423 <sergio.reyes.21m@utzmg.edu.mx>
2025-08-21 14:57:56 -0600

    upgrade CV 3

--- src/components/getPDF/GetPdf.jsx
+++ src/components/getPDF/GetPdf.jsx
@@ -3,7 +3,8 @@ import './GetPdf.css';
 import { useLanguage } from '../../configuration/languages/GlobalLanguages';
 // PDF CV
 import CVEnglish from "../../pdf/CV_english.pdf";
-import cv_current from "../../pdf/my_cv.pdf"; // current CV
+import cv_current from "../../pdf/my_cv.pdf"; 
+import cv_upgrade from "../../pdf/cv_cv_v00.pdf"; // current CV
 
 export default function GetPdf() {
     const { texts } = useLanguage();
@@ -12,7 +13,7 @@ export default function GetPdf() {
         <section id="resume">
             <div className="container-get-pdf">
                 <a
-                    href={cv_current}
+                    href={cv_upgrade}
                     target='_blank'
                     rel='noopener noreferrer'
                     className="shadow__btn"
